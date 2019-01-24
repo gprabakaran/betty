2d4d33ce79cf2f467c7cfafa31dcbfd6f064e13f
Willem Meints <willem.meints@gmail.com>
2019-01-24 11:54:09 +0100

    Setup client app structure

---
+++ src/betty.frontend/Clientapp/webpack.config.js
@@ -0,0 +1,31 @@
+﻿const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: [
+                    /node_modules/,
+                    /webpack.*/
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.ts', '.js']
+    },
+    output: {
+        filename: 'frontend.js',
+        path: path.resolve(__dirname, '../wwwroot')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Improbable spaceflight inc.'
+        })
+    ]
+};
